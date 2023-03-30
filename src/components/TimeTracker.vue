cfab1b5b6ac919413f9f15840d0432d6a69739cf
María Luisa Martínez <mluisamartinezc@gmail.com>
2023-03-30 12:02:43 +0200

    initial design

---
+++ src/components/TimeTracker.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="flex bg-gray-100 rounded-full p-3">
+    <div class="flex-initial w-10 text-black p-3">
+      <span>{{stopwatch.hours}}</span>:<span>{{stopwatch.minutes}}</span>:<span>{{stopwatch.seconds}}</span>
+    </div>
+    <div class="flex-initial w-16 text-slate-300 p-3">
+      /<span>{{stopwatch.hours}}</span>:<span>{{stopwatch.minutes}}</span>:<span>{{stopwatch.seconds}}</span>
+    </div>
+    <button @click="stopwatch.start()" class="bg-green-300 hover:bg-green-400 text-white font-bold py-2 px-4 rounded-full flex-initial w-32 m-1">
+      Entrar
+    </button>
+    <button @click="stopwatch.pause()" class="bg-gray-300 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded-full flex-initial w-32 m-1">
+      Pausar
+    </button>
+    <button @click="stopwatch.reset()" class="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded-full flex-initial w-32 m-1">
+      Salir
+    </button>
+    <div class="flex -space-x-2 overflow-hidden flex-initial w-12 m-1">
+      <img class="inline-block h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+      <span class="top-0 left-7 absolute w-3.5 h-3.5 bg-green-400 rounded-full"></span>
+    </div>
+
+    <Menu as="div" class="relative inline-block text-left flex-initial w-32 m-1">
+    <div>
+      <MenuButton class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-grey px-3 py-2 text-sm font-bold text-gray-900 hover:bg-gray-50">
+        Kelly Pierce
+        <ChevronDownIcon class="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+      </MenuButton>
+    </div>
+
+    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+      <MenuItems class="absolute right-0 z-10 mt-5 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div class="py-1"> 
+          <Menu>         
+            <MenuButton class="inline-flex w-full gap-x-1.5 rounded-md bg-grey px-3 py-2 text-sm text-gray-900 hover:bg-gray-50">
+              <ChevronLeftIcon class="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+              Mis cuentas
+            </MenuButton>
+            <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+              <MenuItems class="absolute right-20 z-10 w-56 origin-top-left divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div class="py-1">
+                  <MenuItem v-slot="{ active }">
+                    <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']" class="font-bold">
+                      Sesame 1 
+                      <span class="font-bold text-slate-400">Kelly Pierce </span> 
+                      <br> 
+                      <span class="text-slate-300">Hoy llevas 00:00</span>
+                    </a>
+                  </MenuItem>
+                </div>
+                <div class="py-1">
+                  <MenuItem v-slot="{ active }">
+                    <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']" class="font-bold">
+                      Sesame 2
+                      <span class="font-bold text-slate-400">Kelly Pierce </span> 
+                      <br> 
+                      <span class="text-slate-300">Hoy llevas 00:00</span>
+                    </a>
+                  </MenuItem>
+                </div>
+              </MenuItems>
+            </transition>
+          </Menu>
+        </div>
+        <div class="py-1">
+          <MenuItem v-slot="{ active }">
+            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Vista empleado</a>
+          </MenuItem>
+        </div>
+        <div class="py-1">
+          <MenuItem v-slot="{ active }">
+            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Mi perfil</a>
+          </MenuItem>
+        </div>
+        <div class="py-1">
+          <MenuItem v-slot="{ active }">
+            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Cerrar sesión</a>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
+  </div>
+</template>
+
+<script setup>
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { ChevronDownIcon, ChevronLeftIcon } from '@heroicons/vue/20/solid'
+import { useStopwatch } from 'vue-timer-hook';
+
+const stopwatch = useStopwatch();
+stopwatch.reset();
+stopwatch.pause();
+</script>
